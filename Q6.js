59b92987bf734539af5bff264c706c739cdedb0b
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 14:08:57 +0500

    Add files via upload
    In this repository I used \t for spacing and \n for move into another line.

---
+++ Q6.js
@@ -0,0 +1,6 @@
+"use strict";
+// \t use for spacing and \n use to move into another line
+let white_space_name = "\n\t Mehwish Shakeel\t\n";
+console.log(white_space_name);
+let withoutwhitespacename = "Mehwish Shakeel";
+console.log(withoutwhitespacename);
---
+++ Q6.ts
@@ -0,0 +1,5 @@
+// \t use for spacing and \n use to move into another line
+let white_space_name:string = "\n\t Mehwish Shakeel\t\n";
+console.log(white_space_name);
+let withoutwhitespacename:string = "Mehwish Shakeel";
+console.log(withoutwhitespacename);
